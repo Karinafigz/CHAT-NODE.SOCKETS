66de54fa27e17f3bbe54679912d2e1adcf7ab9ef
Karinafigz <gfigueroa6@ucol.mx>
2022-03-15 23:41:30 -0600

    primera versión

---
+++ index.js
@@ -0,0 +1,32 @@
+/*let num1=parseInt (prompt("Introduce el primer número"));
+let num2=parseInt(prompt("Introduce el segundo número"));
+
+function suma(num1,num2){
+    let resultado=num1+num2
+    return resultado;
+}
+document.write(suma(num1,num2));
+
+
+const sumar=(num1,num2)=> resultado= num1+num2;
+let mostrar =sumar(num1,num2);
+document.write(mostrar);
+
+const imprimirFrase =  nombre =>'Hola' + nombre;
+let nombre ='Karina Figueroa';
+console.log(imprimirFrase(nombre));*/
+
+
+const imprimirF =nombre => `Hola ${nombre}`;
+let nombres ='Karina Figueroa';
+console.log(imprimirF(nombres));
+
+const carro={
+color: 'rojo',
+marca: 'Toyota',
+funciones:{
+    acelera: 160,
+    frena:0,
+    }
+}
+console.log(carro.color);
